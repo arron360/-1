dbd5cb2ed72ff841ce7101535c776b7aa2f08e89
arron <arronlev@yandex.ru>
2020-04-04 14:25:31 +0300

    Третий комит

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
-
+let money = prompt("Ваш бюджет на месяц?","");
+let	data = prompt("Введите дату в формате YYYY-MM-DD", "");
 let appData = {
 	budget: money,
+	timeData: data,
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	timeData: time,
-	savings: false
+	savings:false
 };
-
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
-
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
-
-alert(appData.budget / 30);
-
+let a1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let	a2 = prompt("Во сколько обойдется?", "");
+let b1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let	b2 = prompt("Во сколько обойдется?", "");
+appData.expenses[a1]=a2;
+appData.expenses[b1]=b2;
+alert(appData.budget/30);
